f67f2356d7d597dc0c28f2140516323edaa1fc7a
Colin de Vries <colindevries325@gmail.com>
2019-12-13 10:06:04 -0800

    completed time complexity questions

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,6 +6,19 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
-    # Step 1: Check if 'th' is in the string
+    matches = []
+    # ** Define inner function to keep track of values in outer function **
 
-    pass
+    def count_th_inner(word):
+        # Step 1: If 'th' is in the word, add word to outer array
+        if 'th' in word:
+            matches.append(word)
+        # Step 2: Find index of 'th' in the word, and slice the word at that index + 2. Repeat Step 1.
+            th_index = word.find('th')
+            new_word = word[th_index + 2:]
+            return count_th_inner(new_word)
+        # Step 3: If 'th' not in word, return length of matches
+        else:
+            return len(matches)
+
+    return count_th_inner(word)
